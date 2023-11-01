76f73f7eab582f8224655fccf6c361e87a09fc15
agusjoffre <aguslj16@gmail.com>
2023-11-01 19:12:23 -0300

    Starting the checkwin funct. Win combinations set

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const Gameboard = (() => {
 
   const buttons = document.querySelectorAll(".btn-grid");
 
+  // Show the board on screen
   const displayBoard = () => {
     buttons.forEach((button, i) => {
       button.textContent = Gameboard.board[i];
     });
   };
 
+  // Every time the buttons are clicked they change the Board array with the symbol of the User clicker. An if statement so it's not posible to rewrite the board once its clicked.
   const userMark = (symbol) => {
     buttons.forEach((button, i) => {
       button.addEventListener("click", () => {
@@ -18,7 +20,7 @@ const Gameboard = (() => {
           Gameboard.board[i] = symbol;
           Gameboard.displayBoard();
         } else {
-          alert('ILEGAL MOVE')
+          alert("ILEGAL MOVE");
         }
       });
     });
@@ -31,9 +33,10 @@ const Gameboard = (() => {
   };
 })();
 
-//Call to mark the board.
-//Despues ponerlo para que se inicialice cada vez que startea el juego.
-Gameboard.userMark("X");
+//Call to mark the board. Defining the Symbol.
+//Despues ponerlo para que se inicialice en cada turno dependiendo el Player y su Simbolo.
+
+Gameboard.userMark("X"); // CADA VEZ QUE CLICKEES VA A SER CON EL SIMBOLO 'X'.
 
 /* Player factory */
 // player creation.
@@ -41,4 +44,29 @@ const playerFactory = (name, symbol) => {};
 
 /* Controller module */
 // turns & checking result.
-const Controller = () => {};
+const Controller = (() => {
+  //Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+  const checkWin = (symbol) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // SI BOARD TIENE ALGUNA DE LAS COMBINACIONES(INDEX) CON EL MISMO SIMBOLO ENTONCES VICTORIA
+    // encontrar forma de asociar los index al board y a simbolo.
+    // desestructurar array?
+    
+
+   
+  };
+  return {
+    checkWin,
+  };
+})();
+
+Controller.checkWin();
